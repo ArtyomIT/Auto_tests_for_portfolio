6966b8411db7bb5b6d867c4ed728770061a00f9d
Artyom <artyom.i.v.35@gmail.com>
2025-10-02 08:12:44 +0300

    Add LoginPage and tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -16,3 +17,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_is_login_form_present(browser):
+    link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_is_registration_form_present(browser):
+    link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+def test_is_substr_login_in_url(browser):
+    link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
